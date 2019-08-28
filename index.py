7c4e12f285cb630ed5c1804f3203f03e98a5ed5a
alvesisaque <isaquealvesdl@gmail.com>
2019-08-27 23:35:21 -0300

    Inserindo algoritmo de ordenação
    Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>

--- index.py
+++ index.py
@@ -2,4 +2,7 @@ import load_and_prepare_data as ld
 
 data = ld.load_and_prepare_data()
 
+
+#dataOrder = ld.selectionSort(data)
+
 print(data)
--- load_and_prepare_data.py
+++ load_and_prepare_data.py
@@ -4,10 +4,27 @@ import json
 
 
 def load_and_prepare_data(file_path=None):
-    file = 'exames.json'
+    file = 'data_set/cnesnone.csv'
     if(file_path):
         file = file_path
     else:
         pass
-    datastore = pd.read_json(file)
+    datastore = pd.read_csv(file)
     return datastore
+
+# primeira opção
+def selectionSort(datastore):
+   for i in range(len(datastore)):
+      # Encontra o elemento mínimo
+       minPosition = i
+    for j in range(i+1, len(datastore)):
+        if datastore[minPosition] > datastore[j]:
+            minPosition = j
+    # Troca o menor elemento encontrado por minPosition
+    temp = datastore[i]
+    datastore[i] = datastore[minPosition]
+    datastore[minPosition] = temp
+    return datastore
+
+# segunda opção
+sortedlist = sorted(data, key=lambda row: row[0], reverse=True)
