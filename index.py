c004fbd1044943b85f7fe2fc2e24138c8068f8df
alvesisaque <isaquealvesdl@gmail.com>
2019-08-28 21:22:24 -0300

    Corrigindo algoritmo de ordenação
    Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>

--- index.py
+++ index.py
@@ -2,7 +2,5 @@ import load_and_prepare_data as ld
 
 data = ld.load_and_prepare_data()
 
-
-#dataOrder = ld.selectionSort(data)
-
-print(data)
+for i in data:
+    print (i)
--- load_and_prepare_data.py
+++ load_and_prepare_data.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 import json
+from operator import itemgetter
 
+# Cabeçalho do dataset 'co_cnes','co_ibge','origem_dado','data_atualizacao','lat','long'
+# Link do dataset http://dados.gov.br/dataset/cnes
 
 def load_and_prepare_data(file_path=None):
     file = 'data_set/cnesnone.csv'
@@ -10,21 +13,6 @@ def load_and_prepare_data(file_path=None):
     else:
         pass
     datastore = pd.read_csv(file)
-    return datastore
-
-# primeira opção
-def selectionSort(datastore):
-   for i in range(len(datastore)):
-      # Encontra o elemento mínimo
-       minPosition = i
-    for j in range(i+1, len(datastore)):
-        if datastore[minPosition] > datastore[j]:
-            minPosition = j
-    # Troca o menor elemento encontrado por minPosition
-    temp = datastore[i]
-    datastore[i] = datastore[minPosition]
-    datastore[minPosition] = temp
-    return datastore
-
-# segunda opção
-sortedlist = sorted(data, key=lambda row: row[0], reverse=True)
+    data = datastore.values.tolist()
+    sortedlist = sorted(data, key=itemgetter(0))
+    return sortedlist
