f6066a9645edc515a0f38e0abdced18fd7a03b08
Guilherme Deusdará <guibanci@gmail.com>
2019-08-31 12:32:24 -0300

    adicionando testes de uniformidade
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- index.py
+++ index.py
@@ -3,4 +3,4 @@ import load_and_prepare_data as ld
 data = ld.load_and_prepare_data()
 
 for i in data:
-    print (i)
+    print(i)
--- load_and_prepare_data.py
+++ load_and_prepare_data.py
@@ -2,9 +2,16 @@ import numpy as np
 import pandas as pd
 import json
 from operator import itemgetter
+import matplotlib.pyplot as plt
+# load the iris dataset
+# separate the data from the target attributes
+# normalize the data attributes
 
 # Cabeçalho do dataset 'co_cnes','co_ibge','origem_dado','data_atualizacao','lat','long'
 # Link do dataset http://dados.gov.br/dataset/cnes
+
+# normalized_data = preprocessing.normalize(sortedlist)
+
 
 def load_and_prepare_data(file_path=None):
     file = 'data_set/cnesnone.csv'
@@ -15,4 +22,7 @@ def load_and_prepare_data(file_path=None):
     datastore = pd.read_csv(file)
     data = datastore.values.tolist()
     sortedlist = sorted(data, key=itemgetter(0))
+
+    numbers = [row[0] for row in data]
+
     return sortedlist
